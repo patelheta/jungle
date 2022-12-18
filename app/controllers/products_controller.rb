7552260866f14c4ea01a54ef4b1d5f3dbc732436
patelheta <patelheta385@gmail.com>
2022-12-17 21:19:38 -0800

    Add Feature: User Authentication

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
 
+  before_action :authorize
   def index
     @products = Product.all.order(created_at: :desc)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,13 @@ Rails.application.routes.draw do
   resources :orders, only: [:create, :show]
 
  
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  
+  get '/signup' => 'user#new'
+  post '/users' => 'user#create'
+
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
